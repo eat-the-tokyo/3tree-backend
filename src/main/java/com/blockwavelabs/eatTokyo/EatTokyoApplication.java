00a5cf16d47276a9f4c01f91254e6b8d5adb33e2
KimHyunju <gkrry27232@naver.com>
2023-04-15 17:06:40 +0900

    initial commit

---
+++ src/main/java/com/blockwavelabs/eatTokyo/EatTokyoApplication.java
@@ -0,0 +1,13 @@
+package com.blockwavelabs.eatTokyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatTokyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatTokyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blockwavelabs/eatTokyo/EatTokyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockwavelabs.eatTokyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatTokyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
