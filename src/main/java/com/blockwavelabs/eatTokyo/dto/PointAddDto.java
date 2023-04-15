09f047c3629946c4dddd013e247e7d3e9eee1196
gkrry2723 <gkrry27232@naver.com>
2023-04-15 20:19:50 +0900

    [Feat] point add dto

---
+++ src/main/java/com/blockwavelabs/eatTokyo/dto/PointAddDto.java
@@ -0,0 +1,11 @@
+package com.blockwavelabs.eatTokyo.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PointAddDto {
+    private Double value;
+    private String userEmail;
+}
