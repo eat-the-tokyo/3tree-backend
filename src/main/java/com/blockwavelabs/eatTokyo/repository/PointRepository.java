65891478a0ee95080e00a2a7921a47a2f07e275a
gkrry2723 <gkrry27232@naver.com>
2023-04-15 18:51:57 +0900

    [Feat] get sum point

--- src/main/java/com/blockwavelabs/eatTokyo/repository/PointRepository.java
+++ src/main/java/com/blockwavelabs/eatTokyo/repository/PointRepository.java
@@ -3,10 +3,15 @@ package com.blockwavelabs.eatTokyo.repository;
 import com.blockwavelabs.eatTokyo.domain.Point;
 import com.blockwavelabs.eatTokyo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PointRepository extends JpaRepository<Point, Long> {
     Optional<List<Point>> findAllByUser(User user);
+
+    @Query("SELECT SUM(p.value) FROM Point p Where p.user = :user")
+    Double sumAllValueByUser(@Param("user") User user);
 }
