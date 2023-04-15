8318406cf8f37ab7248c87e6e45fd1cf8d38c6d3
gkrry2723 <gkrry27232@naver.com>
2023-04-15 17:58:35 +0900

    [Feat] point domain

---
+++ src/main/java/com/blockwavelabs/eatTokyo/domain/Point.java
@@ -0,0 +1,33 @@
+package com.blockwavelabs.eatTokyo.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+public class Point {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_index")
+    private User user;
+
+    private Double value;
+
+    @Builder
+    public Point(User user, Double value) {
+        this.user = user;
+        this.value = value;
+    }
+
+}
